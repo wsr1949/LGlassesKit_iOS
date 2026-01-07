0a3f6aa354b364f4cbcc0b942b1f0ad546ad3ae8
921903719@qq.com <921903719@qq.com>
2026-01-07 16:49:53 +0800

    1.0.5

--- XCFramework/LGlassesKit_iOS.xcframework/ios-arm64_x86_64-simulator/LGlassesKit_iOS.framework/Headers/LGlassesKit_iOS.h
+++ XCFramework/LGlassesKit_iOS.xcframework/ios-arm64_x86_64-simulator/LGlassesKit_iOS.framework/Headers/LGlassesKit_iOS.h
@@ -23,6 +23,9 @@ FOUNDATION_EXPORT const unsigned char LGlassesKit_iOSVersionString[];
  
  版本记录：
  
+ project    2026-01-06  Version:1.0.5   Build:2026010601
+            1.修复已知解析错误BUG
+ 
  project    2026-01-05  Version:1.0.4   Build:2026010501
             1.更新「开始扫描设备」方法 startScanningWithCallback:timeout:broadcastAnalysis: 新增broadcastAnalysis是否解析广播，YES解析成功才回调，NO不解析广播数据直接回调
  
